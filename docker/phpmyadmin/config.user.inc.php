05b343ba2e7b4f33524aeed1fed8d5b48b064a0e
aleks <aleks_st.95@mail.ru>
2021-04-12 06:52:51 -0700

    init commit

---
+++ docker/phpmyadmin/config.user.inc.php
@@ -0,0 +1,13 @@
+<?php
+################################################################################################################
+# https://docs.phpmyadmin.net/en/latest/config.html
+################################################################################################################
+
+# The maximum number of table names to be displayed in the main panel’s list (except on the Export page).
+$cfg['MaxTableList'] = 500;
+
+# The number of items (tables, columns, indexes) that can be displayed on each page of the navigation tree.
+$cfg['MaxNavigationItems'] = 500;
+
+# Defines which type of editing controls should be used for CHAR and VARCHAR columns.
+$cfg['CharEditing'] = 'textarea';
